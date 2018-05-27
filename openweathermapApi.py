afa5cf61b29936aad6d71c953325952de79c54cd
wangdetash <wangdetash@gmail.com>
2018-05-27 19:48:37 +0530

    added coordinate information

--- openweathermapApi.py
+++ openweathermapApi.py
@@ -12,27 +12,38 @@ json_data = requests.get(url).json()
 #print(json_data)
 #print(type(json_data)) #prints the datatype of variable
 
-visibility = json_data['visibility']
 place =  json_data['name']
+print("Place:",place,"\n\r")
 
+print("********coordinates********************************************************")
+coordinates = json_data['coord']
+longitude = coordinates['lon']
+latitude = coordinates['lat']
+print("longitude:",longitude)
+print("latitude:",latitude)
+
+print("********wind***************************************************************")
 wind  = json_data['wind']
 degree = wind['deg']
 speed = wind['speed']
+print("wind:",degree , "deg" , "at", speed ,"km/hr\n\r")
 
+print("*******temperature*********************************************************")
 main = json_data['main']
 temp = main['temp']
 tempMin = main['temp_min']
 tempMax = main['temp_max']
 Humidity = main['humidity']
-
-print("Place:",place,"\n\r")
-
-print("wind:",degree , "deg" , "at", speed ,"km/hr\n\r")
-
 print("current temperature:",temp ,"deg C\n\r")
 print("max temperature:",tempMax , "deg C\n\r")
 print("min temperature:",tempMin , "deg C\n\r")
 print("Humidity:",Humidity ,"%\n\r")
+
+
+
+
+
+
 
 
 
